443b8d5e8a95a66e3b13028a95ea15b2f2bf7da2
Bine Markelj <bine.markelj@gmail.com>
2024-05-21 19:43:22 +0200

    project init + snake game

---
+++ algorithms.py
@@ -0,0 +1,35 @@
+# imports
+
+# TODO: MATJAŽ
+def algorithm_A_star():
+    pass
+
+
+
+# TODO: MATJAŽ
+def algorithm_bfs():
+    pass
+
+
+
+# TODO: BINE
+def algorithm_dfs():
+    pass
+
+
+
+# TODO: BINE
+def algorithm_rta():
+    pass
+
+
+
+# TODO: BINE
+def algorithm_rl():
+    pass
+
+
+
+# TODO: MATJAŽ
+def algorithm_dqn():
+    pass
